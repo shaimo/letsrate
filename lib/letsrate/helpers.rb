55651d9eb861e2793e39cff4e9df245813b193ad
Robert Johansson <robertjo79@gmail.com>
2013-03-25 22:47:53 +0100

    Added hacks for special message when user not logged in.

--- lib/letsrate/helpers.rb
+++ lib/letsrate/helpers.rb
@@ -15,16 +15,13 @@ module Helpers
 
     star = options[:star] || 5
 
-    disable_after_rate = options[:disable_after_rate] || false
+    readonly = !current_user.present?
 
-    readonly = false
-    if disable_after_rate
-      readonly = current_user.present? ? !rateable_obj.can_rate?(current_user.id, dimension) : true
-    end
 
     content_tag :div, '', "data-dimension" => dimension, :class => "star", "data-rating" => avg,
                 "data-id" => rateable_obj.id, "data-classname" => rateable_obj.class.name,
-                "data-disable-after-rate" => disable_after_rate,
+                "data-disable-after-rate" => false,
+                "data-not-rated-text" => options[:not_rated_text], # Only seen when readonly is true
                 "data-readonly" => readonly,
                 "data-star-count" => star
   end
